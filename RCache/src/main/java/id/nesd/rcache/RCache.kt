5deec34c29714cdee7c508b3a385784bffdcd264
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-08-21 18:28:19 +0700

    Complete the RCache

--- RCache/src/main/java/id/nesd/rcache/RCache.kt
+++ RCache/src/main/java/id/nesd/rcache/RCache.kt
@@ -1,5 +1,52 @@
 package id.nesd.rcache
 
-class RCache private constructor(){
+import android.content.Context
+
+/**
+ * This is a class for managing cache
+ */
+object RCache {
+
+    private lateinit var appContext: Context
+
+    /**
+     * Initialize the RCache with the application context.
+     *
+     * @param context The application context.
+     */
+    fun initialize(context: Context) {
+        // Use the application context to prevent memory leaks
+        this.appContext = context.applicationContext
+    }
+
+    /**
+     * Get a RCaching instance with common level. Used to store RCache for regular data.
+     *
+     * @return RCaching
+     *
+     * @note This instance is an instance of SharedPreferencesRCache.
+     */
+    val common: RCaching
+        get() = SharedPreferencesRCache.getInstance(appContext)
+
+    /**
+     * Get a RCaching instance with credentials level. Used to store RCache for credentials data.
+     *
+     * @return RCaching
+     *
+     * @note This instance is an instance of KeychainRCache.
+     */
+    val credentials: RCaching
+        get() = KeychainRCache.getInstance(appContext)
+
+    /**
+     * Method to delete all data for common and credential levels stored via RCache/RCaching.
+     */
+    fun clear() {
+        common.clear()
+        credentials.clear()
+    }
+
+    // MARK: - Key
     class Key(val rawValue: String)
 }
