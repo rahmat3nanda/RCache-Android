6ce462203a79d07488b5f620ac972e61cc7b4433
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-08-21 18:25:42 +0700

    Embed crypto

--- RCache/build.gradle.kts
+++ RCache/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation ("androidx.security:security-crypto:1.1.0-alpha03")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
