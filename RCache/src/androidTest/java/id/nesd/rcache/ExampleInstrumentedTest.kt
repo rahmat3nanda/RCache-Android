0ea225f7e4579dc994997ff0815fa68ae67952a6
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-09-03 02:47:25 +0700

    update test

--- RCache/src/androidTest/java/id/nesd/rcache/ExampleInstrumentedTest.kt
+++ RCache/src/androidTest/java/id/nesd/rcache/ExampleInstrumentedTest.kt
@@ -2,8 +2,10 @@ package id.nesd.rcache
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import com.google.gson.annotations.SerializedName
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -15,55 +17,59 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
 
-    private val data = RCache.Key("data")
+    private val byteArray = RCache.Key("byteArray")
     private val string = RCache.Key("string")
     private val bool = RCache.Key("bool")
     private val int = RCache.Key("int")
     private val array = RCache.Key("array")
-    private val dictionary = RCache.Key("dictionary")
+    private val map = RCache.Key("map")
     private val double = RCache.Key("double")
     private val float = RCache.Key("float")
 
+    @Before
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         RCache.initialize(appContext)
-        testCommon()
-        testCredentials()
     }
 
     @Test
     fun testCommon() {
         val values = Values(
-            data = "RCache-common".toByteArray(),
+            byteArray = "RCache-common".toByteArray(),
             string = "RCache-common",
             bool = false,
             int = 1,
             array = listOf(1, "b", true),
-            dictionary = mapOf("a" to 1, "b" to 2),
+            map = mapOf("a" to 1, "b" to 2),
             double = 12.123,
-            float = 15.554f
+            float = 15.554f,
+            dataClassModel = MyDataClassModel(
+                id = 1,
+                name = "RCache-common",
+                phoneNumber = "081"
+            )
         )
 
-        RCache.common.save(values.data, data)
+        RCache.common.save(values.byteArray, byteArray)
         RCache.common.save(values.string, string)
         RCache.common.save(values.bool, bool)
         RCache.common.save(values.int, int)
         RCache.common.save(values.array, array)
-        RCache.common.save(values.dictionary, dictionary)
+        RCache.common.save(values.map, map)
         RCache.common.save(values.double, double)
         RCache.common.save(values.float, float)
 
         assertTrue(
-            "Common data match",
-            values.data.contentEquals(RCache.common.readData(data))
+            "Common byteArray match",
+            values.byteArray.contentEquals(RCache.common.readByteArray(byteArray))
         )
         assertEquals(values.string, RCache.common.readString(string))
         assertEquals(values.bool, RCache.common.readBool(bool))
         assertEquals(values.int, RCache.common.readInteger(int))
-        assertEquals(values.array.size, RCache.common.readArray(array)?.size)
-        assertEquals(values.dictionary.size, RCache.common.readDictionary(dictionary)?.size)
+        assertEquals(values.array.size, RCache.common.readArray<Any>(array)?.size)
+        assertEquals(values.map.size, RCache.common.readMap<Any>(map)?.size)
         assertEquals(values.double, RCache.common.readDouble(double))
         assertEquals(values.float, RCache.common.readFloat(float))
     }
@@ -71,48 +77,60 @@ class ExampleInstrumentedTest {
     @Test
     fun testCredentials() {
         val values = Values(
-            data = "RCache-credentials".toByteArray(),
+            byteArray = "RCache-credentials".toByteArray(),
             string = "RCache-credentials",
             bool = false,
             int = 1,
             array = listOf(1, "b", true),
-            dictionary = mapOf("a" to 1, "b" to 2),
+            map = mapOf("a" to 1, "b" to 2),
             double = 12.123,
-            float = 15.554f
+            float = 15.554f,
+            dataClassModel = MyDataClassModel(
+                id = 2,
+                name = "RCache-credentials",
+                phoneNumber = "082"
+            )
         )
 
-        RCache.credentials.save(values.data, data)
+        RCache.credentials.save(values.byteArray, byteArray)
         RCache.credentials.save(values.string, string)
         RCache.credentials.save(values.bool, bool)
         RCache.credentials.save(values.int, int)
         RCache.credentials.save(values.array, array)
-        RCache.credentials.save(values.dictionary, dictionary)
+        RCache.credentials.save(values.map, map)
         RCache.credentials.save(values.double, double)
         RCache.credentials.save(values.float, float)
 
         assertTrue(
-            "Credentials data match",
-            values.data.contentEquals(RCache.credentials.readData(data))
+            "Credentials byteArray match",
+            values.byteArray.contentEquals(RCache.credentials.readByteArray(byteArray))
         )
         assertEquals(values.string, RCache.credentials.readString(string))
         assertEquals(values.bool, RCache.credentials.readBool(bool))
         assertEquals(values.int, RCache.credentials.readInteger(int))
-        assertEquals(values.array.size, RCache.credentials.readArray(array)?.size)
-        assertEquals(values.dictionary.size, RCache.credentials.readDictionary(dictionary)?.size)
+        assertEquals(values.array.size, RCache.credentials.readArray<Any>(array)?.size)
+        assertEquals(values.map.size, RCache.credentials.readMap<Any>(map)?.size)
         assertEquals(values.double, RCache.credentials.readDouble(double))
         assertEquals(values.float, RCache.credentials.readFloat(float))
     }
 }
 
+data class MyDataClassModel(
+    val id: Int,
+    val name: String,
+    @SerializedName("phone_number") val phoneNumber: String
+)
+
 data class Values(
-    val data: ByteArray,
+    val byteArray: ByteArray,
     val string: String,
     val bool: Boolean,
     val int: Int,
     val array: List<Any>,
-    val dictionary: Map<String, Any>,
+    val map: Map<String, Any>,
     val double: Double,
-    val float: Float
+    val float: Float,
+    val dataClassModel: MyDataClassModel
 ) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -120,27 +138,29 @@ data class Values(
 
         other as Values
 
-        if (!data.contentEquals(other.data)) return false
+        if (!byteArray.contentEquals(other.byteArray)) return false
         if (string != other.string) return false
         if (bool != other.bool) return false
         if (int != other.int) return false
         if (array != other.array) return false
-        if (dictionary != other.dictionary) return false
+        if (map != other.map) return false
         if (double != other.double) return false
         if (float != other.float) return false
+        if (dataClassModel != other.dataClassModel) return false
 
         return true
     }
 
     override fun hashCode(): Int {
-        var result = data.contentHashCode()
+        var result = byteArray.contentHashCode()
         result = 31 * result + string.hashCode()
         result = 31 * result + bool.hashCode()
         result = 31 * result + int
         result = 31 * result + array.hashCode()
-        result = 31 * result + dictionary.hashCode()
+        result = 31 * result + map.hashCode()
         result = 31 * result + double.hashCode()
         result = 31 * result + float.hashCode()
+        result = 31 * result + dataClassModel.hashCode()
         return result
     }
 }
