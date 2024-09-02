f8f5aa439bcda564321deb6dcbca4cb23e6bcdaa
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-09-03 02:46:57 +0700

    Create encoding

---
+++ RCache/src/main/java/id/nesd/rcache/RCacheEncoding.kt
@@ -0,0 +1,24 @@
+package id.nesd.rcache
+
+import com.google.gson.internal.LinkedTreeMap
+
+internal object RCacheEncoding {
+
+    internal fun ByteArray.toBase64(): String {
+        return android.util.Base64.encodeToString(this, android.util.Base64.DEFAULT)
+    }
+
+    internal fun String.fromBase64(): ByteArray {
+        return android.util.Base64.decode(this, android.util.Base64.DEFAULT)
+    }
+
+    fun convertMap(map: Map<String, Any>): Map<String, Any> {
+        return map.mapValues { entry ->
+            when (val value = entry.value) {
+                is LinkedTreeMap<*, *> -> convertMap(value as Map<String, Any>)
+                is LinkedHashMap<*, *> -> convertMap(value as Map<String, Any>)
+                else -> value
+            }
+        }
+    }
+}
